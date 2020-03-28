ef22d879818eb87070f1e93957309fbd48f68c0e
garrett-ordner <garrettordner@gmail.com>
2020-03-28 12:13:58 -0500

    Update zigzag to use -= operator

--- automate-the-boring-stuff/zigzag.py
+++ automate-the-boring-stuff/zigzag.py
@@ -18,7 +18,7 @@ try:
             if indent == 20:
                 indentIncreasing = False
         else:
-            indent += -1
+            indent -=1
             if indent == 0:
                 indentIncreasing = True
 except KeyboardInterrupt:
